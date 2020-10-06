434cd21fa1fb477084293094051f1e59884cda00
Julio Machado <juliocarlos00@hotmail.com>
2020-10-06 10:57:02 -0300

    First commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn'
+
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+  </Switch>
+);
+
+export default Routes;
